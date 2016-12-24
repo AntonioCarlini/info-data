87187d1611cfa78e64575729c34e1d61d20d8b5f
Antonio Carlini <arcarlini@iee.org>
2016-12-24 22:41:55 +0000

    scripts/systems-yaml-to-infobox-mediawiki.rb: Begin to put in code to build XML suitable for import to mediawiki.
    Make it possible to write out a file that can be used to import into mediawiki.
    To do this wrap everything in <mediawiki> ... </mediawiki> tags.
    
    Each page should be within <page> ... </page> tags. The page text should
    be preceded by a <title> ... <title> and be included in <revision> ... <revision> along with a tags describing the comment, the timestamp and the contributor.
    
    The page text should be mediawiki markup but certain characters (the duoble quote, the ampersand and the angle brackets) need to be escaped to avoid interfering with the XML parsing.

--- scripts/systems-yaml-to-infobox-mediawiki.rb
+++ scripts/systems-yaml-to-infobox-mediawiki.rb
@@ -7,10 +7,29 @@ require_relative "DataTags.rb"
 
 require "yaml"
 
+class Output
+  def initialize(build_xml)
+    @build_xml = build_xml
+  end
+
+  def puts(text="")
+    # First swap out the initial ampersands (as other swaps will put *in* ampersands)
+    # The order of the other substitutions does not currently matter.
+    text = text.gsub(/&/,"&amp;").gsub(%r{\"},"&quot;").gsub(/</,"&lt;").gsub(/>/,"&gt;") if @build_xml
+    $stdout.puts(text)
+  end
+
+  def puts_xml(text)
+    $stdout.puts(text)
+  end
+end
+
 sys_type = ARGV.shift()    # This might be 'vax' or 'alpha' etc.
 sys_yaml = ARGV.shift()    # This is the systems YAML file
 tags_yaml = ARGV.shift()   # This is the tags YAML file
 refs_yaml = ARGV.shift()   # This is the references YAML file
+
+build_xml = true
 
 # Load the references YAML information
 refs = YAML.load_file(refs_yaml)
@@ -21,10 +40,19 @@ systems = YAML.load_file(sys_yaml)
 # Load the supplied tags
 tags = DataTags.new(tags_yaml, 'systems', sys_type)
 
+op = Output.new(build_xml)
+
+# This is the time with which the XML page entries will be stamped
+page_time = Time.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+# The comment will indicate the latest modification time of the source file
+source_file_time = File.mtime(sys_yaml)
+
 # systems will be a hash of {system name => properties-hash} 
 # properties-hash will be {property => array-of-values}
 # array-of-values[0] will be the value, [1] onwards will be references.
 
+op.puts_xml(%q[<mediawiki xml:lang="en">]) if build_xml
 systems.keys().each() {
   |id|
   local_refs = {} # { ref-id => [index, ref-hash-from-yaml] }
@@ -36,9 +64,18 @@ systems.keys().each() {
     next
   end
   system_name = system_name_array[0]
-  puts("== Specifications for #{system_name} ==")
-  puts()
-  puts("{{Infobox#{sys_type.upcase()}-Data")
+  if build_xml
+    op.puts_xml(%Q[  <page>])
+    op.puts_xml(%Q[    <title>#{system_name} systems</title>])
+    op.puts_xml(%Q[    <revision>])
+    op.puts_xml(%Q[      <timestamp>#{page_time}</timestamp>])
+    op.puts_xml(%Q[      <comment>Created from #{File.basename(sys_yaml)}, last modified at #{source_file_time}</comment>])
+    op.puts_xml(%Q[      <contributor><username>antonioc-scripted</username></contributor>])
+    op.puts_xml(%Q[      <text>])
+  end
+  op.puts("== Specifications for #{system_name} ==")
+  op.puts()
+  op.puts("{{Infobox#{sys_type.upcase()}-Data")
   # Work out a plausible name; default to "UNKNOWN"
   d_name = properties["Desc-name"]
   s_name = properties["Sys-name"]
@@ -48,7 +85,7 @@ systems.keys().each() {
     name = s_name[0] unless s_name.nil?()
     name = "UNKNOWN" if name.nil?() || name.empty?()
   end
-  puts("| name = #{name}")
+  op.puts("| name = #{name}")
   properties.keys().each() {
     |prop|
     next if prop =~ /sys-class/i # This should be handled in some special way (VAX4000, VAX6000, UNIBUS etc.)
@@ -72,13 +109,13 @@ systems.keys().each() {
     else
       ref_text = " [[#ref_#{ref_index}|[#{ref_index}]]]"
     end
-    puts("| #{tags[prop].name()} = #{value}#{ref_text}")
+    op.puts("| #{tags[prop].name()} = #{value}#{ref_text}")
   }
-  puts("}}")
-  puts()
+  op.puts("}}")
+  op.puts()
   unless local_refs.empty?()
-    puts("== References ==")
-    puts()
+    op.puts("== References ==")
+    op.puts()
     ref_text_array = []
     local_refs.each() {
       |key, value|
@@ -86,6 +123,13 @@ systems.keys().each() {
       properties = value[1]
       ref_text_array << %Q% <div id="ref_#{index}">[#{index}] #{properties['title']}. #{properties['part-no']}</div>%
     }
-    ref_text_array.sort().each() { |line| puts(line) }
+    ref_text_array.sort().each() { |line| op.puts(line) }
+  end
+  if build_xml
+    op.puts_xml(%q[       </text>])
+    op.puts_xml(%q[    </revision>])
+    op.puts_xml(%q[  </page>])
   end
 }
+
+op.puts_xml(%q[</mediawiki>]) if build_xml
