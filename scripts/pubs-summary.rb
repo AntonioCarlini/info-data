624df902ca041a984f6b7d7e197f6c999e74299f
Antonio Carlini <antonio@acarlini.com>
2024-12-03 17:27:43 +0000

    scripts/pubs-summary.rb: Script to produce 'document=' entries
    This script was written some time ago to take a PUBS file and produce a set of 'document=' entries for each document described in the PUBS file. It helps sometimes when trying to add associated documents to an INFO entry. Commit it as is so that there is a baseline available for further enhancements.

---
+++ scripts/pubs-summary.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require "pathname.rb"
+$LOAD_PATH.unshift(Pathname.new(__FILE__).realpath().dirname().dirname().dirname() + "libs" + "ruby")
+
+require_relative "DataPubTxt.rb"
+
+require "yaml"
+
+info = ARGV.shift()
+
+# Load the refs.info
+pubs = Publications.create_from_info_file(info)
+
+# Output in form suitable for info file
+pubs.each() {
+  |key|
+  pub = pubs[key]
+  next if pub.identifier().nil?() || pub.identifier().empty?()
+  id = "{" + pub.identifier() + "}"
+  if id.length() < 40
+    id = '%-40.40s' % id
+  end
+  puts("document=doc" + id + "! #{pub.title()}")
+}
